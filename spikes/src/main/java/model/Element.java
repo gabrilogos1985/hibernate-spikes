9a2ef2bfdc54104ef3e799b3a57f9835dee872ab
dSony <Sony@VAIO>
2015-03-08 16:38:14 -0600

    move resources to resources folder Add validations to saveMany to Many

--- spikes/src/main/java/model/Element.java
+++ spikes/src/main/java/model/Element.java
@@ -1,22 +1,55 @@
 package model;
 
-public class Element implements Identifier {
-Integer id;
-String name;
-public Integer getId() {
-	return id;
+public class Element implements Identifier, Comparable<Element> {
+    Integer id;
+    String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer idElement) {
+        this.id = idElement;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Element [idElement=" + id + ", name=" + name + "]";
+    }
+
+    @Override
+    public int compareTo(Element o) {
+        if (this.getName() == null) return -1;
+        if (o.getName() == null) return 1;
+        return this.getName().compareTo(o.getName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Element)) return false;
+
+        Element element = (Element) o;
+
+        if (id != null ? !id.equals(element.id) : element.id != null) return false;
+        if (name != null ? !name.equals(element.name) : element.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
-public void setId(Integer idElement) {
-	this.id = idElement;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-@Override
-public String toString() {
-	return "Element [idElement=" + id + ", name=" + name + "]";
-}
-}
+
--- spikes/src/test/java/model/ContainerElementSaveTest.java
+++ spikes/src/test/java/model/ContainerElementSaveTest.java
@@ -1,11 +1,10 @@
 package model;
 
-import java.util.HashSet;
-
 import org.hibernate.Criteria;
 import org.hibernate.FetchMode;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,117 +13,152 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.TreeSet;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:app-context.xml")
 @Transactional
 public class ContainerElementSaveTest {
-	@Autowired
-	private SessionFactory sessionFactory;
-	private int counter;
-	private boolean lazy = true;
+    @Autowired
+    private SessionFactory sessionFactory;
+    private int counter;
+    private boolean lazy = true;
 
-	@Before
-	public void setup() {
-		lazy = true;
-	}
+    @Before
+    public void setup() {
+        lazy = true;
+    }
 
-	@Test
-	public void save() throws Exception {
-		Element element = createElementAndSave();
+    @Test
+    public void saveManyToMany() throws Exception {
+        Element element = createElementAndSave();
+        Container container = new Container();
+        container.setName("First Container");
+        saveObject(container);
 
-		Container container = new Container();
-		container.setName("First Container");
-		saveObject(container);
+        container.setElementos(new HashSet<Element>());
+        container.getElementos().add(element);
+        container = saveObject(container);
+        Iterator<Element> iterator = container.getElementos().iterator();
+        assertThat(iterator.next().getName(), equalTo(element.getName()));
+        Assert.assertFalse(iterator.hasNext());
 
-		container.setElementos(new HashSet<Element>());
-		container.getElementos().add(element);
-		saveObject(container);
+        Element anotherElement = createElementAndSave();
+        container.getElementos().add(anotherElement);
+        TreeSet<Element> sortedSet = new TreeSet<Element>(container.getElementos());
+        iterator = sortedSet.iterator();
+        assertThat(iterator.next().getName(), equalTo(element.getName()));
+        assertThat(iterator.next().getName(), equalTo(anotherElement.getName()));
+        Assert.assertFalse(iterator.hasNext());
 
-		container.getElementos().add(createElementAndSave());
-		saveObject(container);
+        container.getElementos().remove(element);
+        container = saveObject(container);
+        iterator = container.getElementos().iterator();
+        assertThat(iterator.next().getName(), equalTo(anotherElement.getName()));
+        assertThat(iterator.hasNext(), equalTo(false));
 
-		container.getElementos().remove(element);
-		saveObject(container);
+    }
 
-	}
+    @Test
+    public void saveNoLazy() throws Exception {
+        lazy = false;
+        Element element = createElementAndSave();
 
-	@Test
-	public void saveNoLazy() throws Exception {
-		lazy = false;
-		Element element = createElementAndSave();
+        Container container = new Container();
+        container.setName("First Container");
+        saveObject(container);
 
-		Container container = new Container();
-		container.setName("First Container");
-		saveObject(container);
+        container.setElementos(new HashSet<Element>());
+        container.getElementos().add(element);
+        saveObject(container);
 
-		container.setElementos(new HashSet<Element>());
-		container.getElementos().add(element);
-		saveObject(container);
+        container.getElementos().add(createElementAndSave());
+        saveObject(container);
 
-		container.getElementos().add(createElementAndSave());
-		saveObject(container);
+        container.getElementos().remove(element);
+        saveObject(container);
+    }
 
-		container.getElementos().remove(element);
-		saveObject(container);
-	}
+    @Test
+    public void saveStaleObjects() throws Exception {
+        Element element = createElementAndSave();
+        Container container = new Container();
+        container.setName("First Container");
+        container = saveObject(container);
 
-	@Test
-	public void saveStaleObjects() throws Exception {
-		Element element = createElementAndSave();
-		Container container = new Container();
-		container.setName("First Container");
-		saveObject(container);
+        Container containerStale = new Container();
+        containerStale.setId(container.getId());
+        containerStale.setName(container.getName());
+        containerStale.setElementos(new HashSet<Element>());
+        Element staleElement = new Element();
+        staleElement.setId(element.getId());
+        staleElement.setName(System.currentTimeMillis() + "");
+        containerStale.getElementos().add(staleElement);
+        saveObject(containerStale);
 
-		Container containerStale = new Container();
-		containerStale.setId(container.getId());
-		containerStale.setName(container.getName());
-		containerStale.setElementos(new HashSet<Element>());
-		Element staleElement = new Element();
-		staleElement.setId(element.getId());
-		staleElement.setName(System.currentTimeMillis() + "");
-		containerStale.getElementos().add(staleElement);
-		saveObject(containerStale);
+        containerStale.getElementos().add(createElementAndSave());
+        saveObject(containerStale);
 
-		containerStale.getElementos().add(createElementAndSave());
-		saveObject(containerStale);
+        Element elementToRemove = (Element) containerStale.getElementos()
+                .toArray()[1];
+        containerStale.getElementos().remove(elementToRemove);
+        saveObject(containerStale);
+    }
 
-		Element elementToRemove = (Element) containerStale.getElementos()
-				.toArray()[1];
-		containerStale.getElementos().remove(elementToRemove);
-		saveObject(containerStale);
-	}
+    private Element createElementAndSave() {
+        Element element = createElement();
+        saveObject(element);
+        return element;
+    }
 
-	private Element createElementAndSave() {
-		Element element = createElement();
-		saveObject(element);
-		return element;
-	}
+    private Element createElement() {
+        Element element = new Element();
+        element.setName("Element" + ++counter);
+        return element;
+    }
 
-	private Element createElement() {
-		Element element = new Element();
-		element.setName("Element" + ++counter);
-		return element;
-	}
+    private <T extends Identifier> T saveObject(T container) {
+        sessionFactory.getCurrentSession().saveOrUpdate(container);
+        flushAndClearSession();
+        container = getEntityWithCriteria(container);
 
-	private void saveObject(Identifier container) {
-		sessionFactory.getCurrentSession().saveOrUpdate(container);
-		sessionFactory.getCurrentSession().flush();
-		sessionFactory.getCurrentSession().clear();
-		container = (Identifier) sessionFactory.getCurrentSession().get(
-				container.getClass(), ((Identifier) container).getId());
-		Criteria add = sessionFactory.getCurrentSession()
-				.createCriteria(container.getClass())
-				.add(Restrictions.eq("id", container.getId()));
-		if (lazy) {
-			add.setFetchMode("elementos", FetchMode.LAZY);
-			container = (Identifier) add.uniqueResult();
-		} else {
-			add.setFetchMode("elementos", FetchMode.JOIN);
-			container = (Identifier) add.uniqueResult();
-			sessionFactory.getCurrentSession().clear();
-		}
+        System.out.println(container);
+        sessionFactory.getCurrentSession().clear();
+        return container;
+    }
 
-		System.out.println(container);
-		sessionFactory.getCurrentSession().clear();
-	}
+    public void flushAndClearSession() {
+        sessionFactory.getCurrentSession().flush();
+        sessionFactory.getCurrentSession().clear();
+    }
+
+    private <T extends Identifier> T getEntityWithCriteria(T container) {
+        Criteria add = getCriteria(container);
+        if (lazy) {
+            add.setFetchMode("elementos", FetchMode.LAZY);
+            container = (T) add.uniqueResult();
+        } else {
+            add.setFetchMode("elementos", FetchMode.JOIN);
+            container = (T) add.uniqueResult();
+            sessionFactory.getCurrentSession().clear();
+        }
+        return container;
+    }
+
+    private Criteria getCriteria(Identifier container) {
+        return sessionFactory.getCurrentSession()
+                .createCriteria(container.getClass())
+                .add(Restrictions.eq("id", container.getId()));
+    }
+
+    private <T> T getEntity(T container, Serializable id) {
+        return (T) sessionFactory.getCurrentSession().get(
+                container.getClass(), id);
+    }
 }
